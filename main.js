99143fba49ea6fd348738bca96faf9828bcd1bc0
AIADO <140124573+AIADO@users.noreply.github.com>
2023-07-21 02:44:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,345 @@
+
+
+
+// 1
+
+// let num=window.prompt()
+// document.write(num)
+
+// 1
+
+
+
+// 2
+
+// let num=window.prompt()
+// if(num%3==0 && num%4==0){
+// document.write("yes")
+// } else{document.write("no")}
+
+// 2
+
+
+
+// 3
+// let num1 = window.prompt()
+// let num2 = window.prompt()
+// if (num1 > num2) { document.write(num1) }
+// else if (num2 > num1) { document.write(num2) }
+// else{document.write("equal")}
+// 3
+
+
+// 4
+// let num=window.prompt()
+// if(num>0){document.write("positive")}
+// else if (num<0){document.write("negative")}
+// 4
+
+
+// 5
+
+// let num1=window.prompt()
+// let num2=window.prompt()
+// let num3=window.prompt()
+// numbers =Math.max(num1,num2,num3)
+// numbers2 =Math.min(num1,num2,num3)
+// document.write(`max num is ${numbers}  ` )
+// document.write(` min num is ${numbers2}`)
+
+// 5
+
+
+// 6
+
+// let num = window.prompt()
+// if (num % 2 == 0) {
+//     document.write(`${num} is even`)
+// } else if (num % 2 != 0) {
+//     document.write(`${num} is odd`)
+// }
+
+// 6
+
+
+// 7
+
+// let cha=prompt()
+// if(cha=="a" || cha=="e" || cha=="i" || cha=="o" || cha=="u" ){
+//     document.write("vowel")
+// }else{
+//     document.write("Consonant")
+// }
+
+// 7
+
+
+// 8
+
+// let num=prompt()
+// for (let i = 1; i <= num; i++) {
+//    console.log( i )
+// }
+
+// 8
+
+
+
+
+// 9
+
+// let num=prompt()
+// for (let i = 1; i <= 12; i++) {
+//    console.log( num*i )
+// }
+
+// 9
+
+
+// 10
+
+// let num=prompt()
+// for (let i = 2; i <= num; i+=2) {
+//    console.log( i )
+// }
+
+// 10
+
+
+// 11
+
+// let num1=prompt()
+// let num2=prompt()
+// console.log(num1**num2)
+
+// 11
+
+
+
+// 12
+
+// let num1 = 95
+// let num2 = 76
+// let num3 = 58
+// let num4 = 90
+// let num5 = 89
+// let sum = num1 + +num2 + num3 + num4 + num5
+// console.log(`sum is ${sum}`)
+// console.log(`Average is ${sum/5}`)
+// console.log(`Percentage is ${sum*100/500}`)
+
+// 12
+
+
+
+// 13
+
+// let month = prompt()
+
+// if (month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11 || month == 12) {
+//     console.log(`days in month are ${31}`)
+
+// } else if (month == 4 || month == 6 || month == 8 || month == 10) {
+//     console.log(`days in month are ${30}`)
+
+// } else if (month == 2) { console.log("days in month are 28 or 29") }
+
+// else { console.log("not a month") }
+
+// 13
+
+
+// 14
+
+// let Physics = prompt("enter your marks in Physics ")
+// let Chemistry = prompt("enter your marks in Chemistry ")
+// let Biology = prompt("enter your marks in Biology ")
+// let Mathematics = prompt("enter your marks in Mathematics ")
+// let Computer = prompt("enter your marks in Computer ")
+// sum=+Physics + +Chemistry + +Biology + +Mathematics + +Computer
+// if(sum>=450 && sum<=500){
+//     console.log(`your rate is ${sum*100/500}% and Grade A`)
+
+// }else if (sum<450 && sum>=400){
+//     console.log(`your rate is ${sum*100/500}% and Grade B`)
+// }
+// else if (sum<400 && sum>=350){
+//     console.log(`your rate is ${sum*100/500}% and Grade C`)
+// }
+// else if (sum<350 && sum>=300){
+//     console.log(`your rate is ${sum*100/500}% and Grade D`)
+// }
+// else if (sum<300 && sum>=250){
+//     console.log(`your rate is ${sum*100/500}% and Grade E`)
+// }
+// else if (sum<250 && sum>=200){
+//     console.log(`your rate is ${sum*100/500}% and Grade F `)
+// }
+// else  { console.log(`your rate is ${sum*100/500}% and Grade H `)}
+
+// 14
+
+
+
+// 1 switch
+
+// let month = prompt("enter month number")
+// switch (month) {
+//     case "1":
+//         document.write("31 days in this month")
+//         break;
+//     case "3":
+//         document.write("31 days in this month")
+//         break;
+//     case "5":
+//         document.write("31 days in this month")
+//         break;
+//     case "7":
+//         document.write("31 days in this month")
+//         break;
+//     case "9":
+//         document.write("31 days in this month")
+//         break;
+//     case "11":
+//         document.write("31 days in this month")
+//         break;
+//     case "12":
+//         document.write("31 days in this month")
+//         break;
+//     case "4":
+//         document.write("30 days in this month")
+//         break;
+//     case "6":
+//         document.write("30 days in this month")
+//         break;
+//     case "8":
+//         document.write("30 days in this month")
+//         break;
+//     case "10":
+//         document.write("30 days in this month")
+//         break;
+//     case "2":
+//         document.write("28 or 29 days in this month")
+//         break;
+//         default: document.write("not a month");
+// }
+
+// 1 switch
+
+
+// 2 switch
+
+// let chra=prompt()
+// switch(chra){
+//     case "a":
+//         document.write("vowel")
+//         break;
+//     case "u":
+//         document.write("vowel")
+//         break;
+//     case "e":
+//         document.write("vowel")
+//         break;
+//     case "i":
+//         document.write("vowel")
+//         break;
+//     case "u":
+//         document.write("vowel")
+//         break;
+//         default:document.write("consnant")
+// }
+
+// 2 switch
+
+
+// 3 switch
+
+// let num1=prompt("num1")
+// let num2=prompt("num2")
+// switch(true){
+//     case(num1>num2):
+//     console.log(`${num1} is bigger`)
+//     break;
+//     case(num1<num2):
+//     console.log(`${num2} is bigger`)
+//     break;
+//     default: console.log("equal")
+// }
+
+// 3 switch
+
+
+// 4 switch
+
+// let num=prompt("num")
+// switch(true){
+//     case (num%2==0):
+//         console.log(`${num} is even`)
+//         break;
+//     case (num%2!=0):
+//         console.log(`${num} is odd`)
+//         break;      
+// }
+
+// 4 switch
+
+
+
+// 5 switch
+
+// let num =prompt()
+// switch(true){
+//     case (num>0):
+//         console.log(`${num} is positive`)
+//         break;
+//     case (num<0):
+//         console.log(`${num} is negative`)
+//         break;
+//     case (num==0):
+//         console.log(`num is ${num}`)
+//         break;
+// }
+
+// 5 switch
+
+
+
+
+// 6 switch
+
+// let Operator = prompt("please enter the Operator you want ( + , - , * , / , **  )")
+// let num1 = prompt("please enter num1")
+// let num2 = prompt("please enter num2")
+// switch (Operator) {
+//     case "+":
+//         console.log(+num1 + +num2)
+//         break;
+//     case "-":
+//         console.log(num1 - num2)
+//         break;
+//     case "*":
+//         console.log(num1 * num2)
+//         break;
+//     case "/":
+//         console.log(num1 / num2)
+//         break;
+//     case "**":
+//         console.log(num1 ** num2)
+//         break;
+//     default: console.log("please choose a right Operator")
+// }
+
+// 6 switch
+
+
+
+
+
+
+
+
+
+
+
+
+
